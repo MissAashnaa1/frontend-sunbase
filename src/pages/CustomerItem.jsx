b3bde167feeeafbb2d050bd59fc81c9e82c12311
MissAashnaa1 <97970004+MissAashnaa1@users.noreply.github.com>
2023-08-04 13:10:15 +0530

    Update CustomerItem.jsx

--- src/pages/CustomerItem.jsx
+++ src/pages/CustomerItem.jsx
@@ -15,16 +15,10 @@ import axios from "axios";
 import BASE_URL from "../constant";
 
 const CustomerItem = ({ cust }) => {
-  // const dispatch = useDispatch();
-  // const [checked, setChecked] = useState(false);
-  const { showCustList } = useSelector((state) => state.counter);
-
-  // useEffect(() => {
-  //   setChecked(task.status);
-  // }, [task.status]);
-
+  const dispatch = useDispatch();
+  const { showCustList, custData } = useSelector((state) => state.counter);
   const handleDelete = async (uuid) => {
-    // dispatch(setDltId(id));
+    dispatch(setDltId(uuid));
     console.log(uuid, "delete uuid");
     let token = localStorage.getItem("loginData");
     token = JSON.parse(token);
@@ -46,106 +40,15 @@ const CustomerItem = ({ cust }) => {
   };
 
   const handleEdit = async (uuid) => {
-    // dispatch(setDltId(id));
-    console.log(id, "edit");
-
-    // try {
-    //   let res = await axios.get(`http://localhost:5000/api/task/${id}`);
-    //   // console.log(res.data);
-    //   if (res.data.success) {
-    //     // dispatch(setEditTaskObj(res.data.task));
-    //     // dispatch(setUpdateID(id));
-    //     // dispatch(setIsUpdate(true));
-    //     // dispatch(setShowTastList(!showTaskList));
-    //   } else {
-    //     toast.error("Something went wrong!");
-    //   }
-    // } catch (err) {
-    //   console.log(err);
-    // }
+    console.log(uuid, "edit");
+    let custObj = custData.filter((cust) => cust.uuid === uuid);
+    // console.log(custObj, "ASdfasdfasdf");
+    dispatch(setUpdateID(uuid));
+    dispatch(setIsUpdate(true));
+    dispatch(setShowCustList(!showCustList));
+    dispatch(setEditCustObj(custObj[0]));
   };
-
-  const handleStatus = async (id, status) => {
-    // try {
-    //   let res = await axios.put(`http://localhost:5000/api/set-status/`, {
-    //     id: id,
-    //     status: status,
-    //   });
-    //   console.log(res.data);
-    // } catch (err) {
-    //   console.log(err);
-    // }
-  };
-
   return (
-    // <div
-    //   id={task._id}
-    //   style={{
-    //     display: "flex",
-    //     flexDirection: "row",
-    //     justifyContent: "space-between",
-    //     alignItems: "center",
-    //     width: "90%",
-    //     marginBottom: "2em",
-    //     padding: "1em",
-    //     backgroundColor: "#d697ff",
-    //     boxSizing: "border-box",
-    //     borderRadius: "1em",
-    //   }}
-    // >
-    //   <div>
-    //     <p className={checked ? "lineThrough" : ""}>
-    //       <span>Title:</span> {task.title}
-    //     </p>
-    //     <p>
-    //       <span>Description:</span> {task.description}
-    //     </p>
-    //     <p>
-    //       <span>Status:</span> {checked ? "Completed" : "Incomplete"}
-    //     </p>
-    //   </div>
-    //   <Box display="flex" alignItems="center" justifyContent="center" gap="4">
-    //     <Checkbox
-    //       size={"lg"}
-    //       isChecked={checked}
-    //       onChange={(e) => {
-    //         setChecked(e.target.checked);
-    //         handleStatus(task._id, e.target.checked);
-    //       }}
-    //     />
-    //     <WrapItem>
-    //       <Button
-    //         onClick={() => {
-    //           handleDelete(task._id);
-    //         }}
-    //         colorScheme="red"
-    //       >
-    //         <DeleteIcon />
-    //       </Button>
-    //     </WrapItem>
-    //     <WrapItem>
-    //       <Button
-    //         onClick={() => {
-    //           handleEdit(task._id);
-    //         }}
-    //         colorScheme="blue"
-    //       >
-    //         <EditIcon />
-    //       </Button>
-    //     </WrapItem>
-    //   </Box>
-    //   <Toaster
-    //     toastOptions={{
-    //       // Define default options
-    //       className: "",
-    //       duration: 3000,
-    //       style: {
-    //         background: "#363636",
-    //         color: "#fff",
-    //       },
-    //     }}
-    //   />
-    // </div>
     <Tr>
       <Td>{cust.first_name}</Td>
       <Td>{cust.last_name}</Td>
